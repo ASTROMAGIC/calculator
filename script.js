fbddf1de9783e51f67b733acca9093b742906f92
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-07-30 10:14:12 -0700

    basic css written
    functional css for project written

---
+++ script.js
@@ -0,0 +1,5 @@
+// basic calculator prograqm 
+
+const display = document.getElementById("display");
+
+// you need 3 basic functions: a calculate or evaluate function, an appendtodisplay function, and a clearDisplay function 

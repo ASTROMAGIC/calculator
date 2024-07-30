ebf846e66e53d0dcc3b34fdcfc53adebcc173ce4
ASTROMAGIC <97130008+ASTROMAGIC@users.noreply.github.com>
2024-07-30 10:25:34 -0700

    script is written but needs adjustment
    will absolutely not be using eval to complete this calculator, will need more writing and adjustment

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 const display = document.getElementById("display");
 
 // you need 3 basic functions: a calculate or evaluate function, an appendtodisplay function, and a clearDisplay function 
+
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function calculate() {
+    display.value
+}
